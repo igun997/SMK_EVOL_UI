cdd611c8792a0b852a97bbb9f01d554e5c4755ba
Indra Gunanda <indra@imceria.com>
2020-05-26 19:11:10 +0700

    ujian_detail.html

--- app.js
+++ app.js
@@ -54,6 +54,25 @@
     return boiler;
   }
 
+  function butir(d) {
+    html = [];
+    for (var i = 0; i < d.length; i++) {
+      let name = 'jawaban['+d[i].id+']';
+      if (d[i].tipe === "pg") {
+        pg_a = d[i].pg_a;
+        pg_b = d[i].pg_b;
+        pg_c = d[i].pg_c;
+        pg_d = d[i].pg_d;
+        pg_e = d[i].pg_e;
+        temp = '<div class="card" style="padding:5px 5px 5px"><p>'+d[i].soal+'</p><label class="label-radio item-content"><input type="radio" name="'+name+'" value="A"><div class="item-inner"><div class="item-title">A. '+pg_a+'</div></div></label><label class="label-radio item-content"><input type="radio" name="'+name+'" value="B"><div class="item-inner"><div class="item-title">B. '+pg_b+'</div></div></label><label class="label-radio item-content"><input type="radio" name="'+name+'" value="C"><div class="item-inner"><div class="item-title">C. '+pg_c+'</div></div></label><label class="label-radio item-content"><input type="radio" name="'+name+'" value="D"><div class="item-inner"><div class="item-title">D. '+pg_d+'</div></div></label><label class="label-radio item-content"><input type="radio" name="'+name+'" value="E"><div class="item-inner"><div class="item-title">E. '+pg_e+'</div></div></label></div>';
+      }else {
+        temp = '<div class="card" style="padding:5px 5px 5px"><p>'+d[i].soal+'</p><textarea name="'+name+'" class="form_textarea" rows="" cols=""></textarea></div>';
+      }
+      html.push(temp);
+    }
+    return html.join("");
+  }
+
   const loginMenus = [
     {
       "name":"Beranda",
@@ -131,6 +150,56 @@
         toastr.error("Warning Config File not found");
       });
     });
+    this.get("/#/ujian/:id", function(context) {
+      context.app.swap('');
+      h = context.render("pages/ujian_detail.html",{}).appendTo(context.$element());
+      $.getScript('./config.js', function() {
+        setTimeout(function () {
+          menuRoll();
+          const id_ujian = context.params.id;
+          let qs = $.param(JSON.parse(localStorage.getItem("info")));
+          let check = localStorage.getItem("soal");
+          if (check === undefined || check === null){
+            localStorage.setItem("soal",JSON.stringify([]));
+
+          }
+          check = JSON.parse(localStorage.getItem("soal"));
+          toastr.info("Data Disiapkan . . .");
+          $("#ujiandetail_pin").find("button[type=submit]").attr("disabled",true);
+          $.get(url+"api/listsoal/"+id_ujian+"?"+qs,function (r) {
+            check[id_ujian] = r.data[id_ujian];
+            localStorage.setItem("soal",JSON.stringify(check));
+            toastr.success("Data Telah Siap");
+            $("#ujiandetail_pin").find("button[type=submit]").attr("disabled",false);
+          });
+          setTimeout(function () {
+            const item = (JSON.parse(localStorage.getItem("soal")))[id_ujian];
+            $("#ujiandetail_pin").on("submit",function () {
+              const own = parseInt($(this).find("input[name=pin]").val());
+              const pin = item.pin;
+              if (own == pin){
+                toastr.success("PIN Benar , Tunggu Anda Akan di Alihkan");
+                setTimeout(function () {
+                  $("#pin_accord").hide().fadeOut().animate();
+                  let form = [
+                      "<form action='' method='post' onsubmit='return false'>",
+                      butir(item.soal),
+                      "</form>",
+                  ];
+                  $("#ujiandetail_space").html(form.join(""));
+                  $("#ujiandetail_space").show().fadeIn().animate();
+                },500);
+              }else {
+                toastr.warning("Maaf PIN Salah");
+              }
+            });
+          },1000)
+
+        }, 1000);
+      }).fail(function(){
+        toastr.error("Warning Config File not found");
+      });
+    });
 
     this.get("/#/ujian", function(context) {
       context.app.swap('');
@@ -145,7 +214,7 @@
             }
             r.data.forEach((i,k)=>{
               let btn = $(i[5]).data("id");
-              btn = "<button class='btn btn-primary unduh' data-id='"+btn+"'>Unduh</button>";
+              btn = "<button class='btn btn-primary unduh' data-id='"+btn+"'>Kerjakan</button>";
               table.append(([
                 "<tr>",
                 "<td>"+i[0]+"</td>",
@@ -157,6 +226,16 @@
                 "</tr>",
               ]).join(""));
             });
+
+            table.on("click",".unduh",function () {
+              console.log("Unduh");
+              toastr.success("Tunggu anda akan di alihkan ");
+              let id = $(this).data("id");
+              setTimeout(function () {
+                console.log(id)
+                location.href="#/ujian/"+id;
+              },1000);
+            })
           });
           menuRoll();
           $("#ujian_reload").on("click",function () {
@@ -180,6 +259,7 @@
               });
             });
           })
+
         }, 1000);
       }).fail(function(){
         toastr.error("Warning Config File not found");
